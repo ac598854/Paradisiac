b46ea1f515d364a45b3ebb13fbde57083ddfa2ab
BOT <a3325yyuacee@gmail.com>
2023-10-30 16:13:38 +0800

    新增 Application 測試 Hello

---
+++ src/main/java/com/paradisiac/product/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.paradisiac.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMallApplication.class, args);
+	}
+
+}
