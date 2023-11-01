5eca1034b7a20e104ea50f91eec715ca6da346dc
BOT <a3325yyuacee@gmail.com>
2023-11-01 17:56:37 +0800

    修改 ProductCategory檔案註解

--- src/main/java/com/paradisiac/productcategory/rowmapper/ProductCategoryRowMapper.java
+++ src/main/java/com/paradisiac/productcategory/rowmapper/ProductCategoryRowMapper.java
@@ -1,29 +1,29 @@
-package com.paradisiac.productcategory.rowmapper;
-
-import com.paradisiac.productcategory.model.ProductCategory;
-import org.springframework.jdbc.core.RowMapper;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class ProductCategoryRowMapper implements RowMapper<ProductCategory> {
-
-    @Override
-    public ProductCategory mapRow(ResultSet rs, int rowNum) throws SQLException {
-        ProductCategory productCategory = new ProductCategory();
-
-        productCategory.setProductCategoryNo(rs.getInt("product_category_no"));
-        productCategory.setProductCategoryName(rs.getString("product_category_name"));
-        productCategory.setProductCategoryDesc(rs.getString("product_category_desc"));
-
-        return productCategory;
-    }
-}
-
-//    // String 轉 Enum
-//    String categoryStr = rs.getString("category");
-//    ProductCategory category = ProductCategory.valueOf(categoryStr);
-//        product.setCategory(category);
+//package com.paradisiac.productcategory.rowmapper;
 //
-//// 簡化寫法
-////        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+//import com.paradisiac.productcategory.model.ProductCategory;
+//import org.springframework.jdbc.core.RowMapper;
+//
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//public class ProductCategoryRowMapper implements RowMapper<ProductCategory> {
+//
+//    @Override
+//    public ProductCategory mapRow(ResultSet rs, int rowNum) throws SQLException {
+//        ProductCategory productCategory = new ProductCategory();
+//
+//        productCategory.setProductCategoryNo(rs.getInt("product_category_no"));
+//        productCategory.setProductCategoryName(rs.getString("product_category_name"));
+//        productCategory.setProductCategoryDesc(rs.getString("product_category_desc"));
+//
+//        return productCategory;
+//    }
+//}
+//
+////    // String 轉 Enum
+////    String categoryStr = rs.getString("category");
+////    ProductCategory category = ProductCategory.valueOf(categoryStr);
+////        product.setCategory(category);
+////
+////// 簡化寫法
+//////        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
