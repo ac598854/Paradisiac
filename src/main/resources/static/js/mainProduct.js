82d4c177b138b1463731b7649afd0e1bfffd8e13
BOT <a3325yyuacee@gmail.com>
2023-11-15 00:27:12 +0800

    更改購物商城header,footer(商城首頁還未更動，要處理購物車)

--- src/main/resources/static/js/mainProduct.js
+++ src/main/resources/static/js/mainProduct.js
@@ -2,6 +2,37 @@
 let pathName = window.document.location.pathname;
 //==================================從路徑名稱中提取出項目名稱==============================================
 let projectName = pathName.substring(0, pathName.substring(1).indexOf("/") + 1);
+
+//==================================取得會員servlet URL==============================================
+var contextPath = window.location.pathname.substring(0, window.location.pathname.indexOf("/",1));
+
+$(document).ready(function(){
+    // 加載頁尾
+    $("#footer").load("http://localhost:8081/Paradisiac/front-end/index/footer.jsp");
+
+    // 處理會員登入
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8081" + projectName + "/front-end/members/members.do?action=indexLogin",
+        success: function(data) {
+            // ... 登入邏輯
+            const responseMessage = parseInt(data);
+            var guided = contextPath + '/front-end/index/guided.jsp';
+            var guidedSignout= contextPath + '/front-end/index/guidedSignout.jsp';
+            if (responseMessage === 1) {
+                $("#dynamicContent").load(guided);
+            } else if (responseMessage === 0) {
+
+                $("#dynamicContent").load(guidedSignout);
+            }
+        },
+        error: function(error) {
+            console.log("AJAX error:", error);
+        }
+    });
+});
+
+
 //==================================當頁面加載時取得productId並調用API==============================================
 document.addEventListener('DOMContentLoaded', function() {
 
